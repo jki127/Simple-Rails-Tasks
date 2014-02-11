e416f5e5698b57a402c8aca6682faed8bc06fef0
Jayson Isaac <jayson@challengepost.com>
2014-02-11 15:27:57 -0500

    First Commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,33 @@
 class TasksController < ApplicationController
   def index
+    @task = Task.new
+  	@tasks = Task.all
+  end
+
+  def create
+  	Task.create task_name
+  	redirect_to :back
+  end
+
+  def task_name
+  	params.require(:task).permit(:task)
+  end
+  
+  def edit
+  	@task = Task.find params[:id]
+  end
+  
+  def update
+    task = Task.find params[:id]
+    if task.update_attributes(params[:task].permit(:task))
+      redirect_to tasks_path, :notice => "Your task was sucessfully updated"
+    else
+      redirect_to :back, :notice => "There was an error in creating updating your task"
+    end
+  end
+  
+  def destroy
+    Task.destroy params[:id]
+    redirect_to :back, :notice => "Task has been deleted"
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,9 @@ require 'test/unit'
 
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'capybara/rails'
+require 'capybara/rspec'
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
@@ -14,6 +17,7 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 RSpec.configure do |config|
+  config.include Capybara::DSL
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
